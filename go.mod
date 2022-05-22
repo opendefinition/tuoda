b8ac21678899570e00dc9f3258763e7165aa892c
rjohnsen <rogercbjohnsen@gmail.com>
2022-05-22 12:17:01 +0100

    Implementing Kong argument parser

--- go.mod
+++ go.mod
@@ -1,3 +1,8 @@
 module github.com/opendefinition/tuoda
 
 go 1.18
+
+require (
+	github.com/alecthomas/kong v0.5.0 // indirect
+	github.com/pkg/errors v0.9.1 // indirect
+)
--- main.go
+++ main.go
@@ -2,9 +2,38 @@ package main
 
 import (
 	"fmt"
+
+	"github.com/alecthomas/kong"
 )
+
+type Context struct {
+	Debug bool
+}
+
+type ParseCmd struct {
+	Parser  string `help:"Path to parser definition"`
+	LogFile string `help:"Path to log file or"`
+}
+
+func (pc *ParseCmd) Run(ctx *Context) error {
+	fmt.Println(pc.Parser)
+	fmt.Println(pc.LogFile)
+
+	return nil
+}
+
+var cli struct {
+	Debug bool `help:"Enable debug mode"`
+
+	Pc ParseCmd `cmd: help:"Parse log"`
+}
 
 func main() {
 	fmt.Println("Tuoda Log Importer - v.0.0.1")
 	fmt.Println("By Roger Johnsen - Opendefinition 2022")
+
+	ctx := kong.Parse(&cli)
+	err := ctx.Run(&Context{Debug: cli.Debug})
+	ctx.FatalIfErrorf(err)
+
 }
